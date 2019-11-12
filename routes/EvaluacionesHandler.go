07a1a998c82c5f289790c8c4e32b1eb603787f9b
Saul Ramirez <saulr@banpais.hn>
2019-11-12 12:57:21 -0600

    cambios generales

--- routes/EvaluacionesHandler.go
+++ routes/EvaluacionesHandler.go
@@ -38,11 +38,6 @@ func GetEvaluacionPorColaborador(w http.ResponseWriter, r *http.Request) {
 
 func GetEvaluacionAnual(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Header().Set("Access-Control-Expose-Headers: Content-Length", "X-JSON")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Content-type", "Application/json")
-
 	vars := mux.Vars(r)
 	idColaborador := vars["idColaborador"]
 
